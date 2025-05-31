fe56d1d1ead01ee2831ddf957170c5225d2b8d28
yethikrishna <105283902+yethikrishna@users.noreply.github.com>
2025-05-31 11:36:37 +0530

    Update Next.js config to ignore frontend directory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+  // Exclude frontend directory from compilation
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  webpack: (config, { isServer }) => {
+    // Ignore files in frontend directory during compilation
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/frontend/**', '**/node_modules/**']
+    };
+    
+    return config;
+  }
 }
 
 module.exports = nextConfig
